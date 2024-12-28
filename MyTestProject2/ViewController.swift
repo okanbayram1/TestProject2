25ab4184cffb27c2e36b73b8b4e76ceac5e4288a
Okan Bayram <okanbayram85@icloud.com>
2024-12-28 18:54:46 +0300

    Initial Commit

---
+++ MyTestProject2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTestProject2
+//
+//  Created by okan bayram on 28.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let myArray = [10,20,30,40,50,60]
+        
+        for num in myArray {
+            print(num)
+        }
+    }
+
+
+}
+
